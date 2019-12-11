c867ede5b1cc3583bb77e3cdd6837292a6fbba22
gastroad <gostroad37@gmail.com>
2019-12-11 23:14:34 +0900

    header 추가 개발

--- front/src/components/Header/Header.tsx
+++ front/src/components/Header/Header.tsx
@@ -13,6 +13,7 @@ const Header = () => {
       >
         즐겨찾기
       </div>
+
       <div
         style={{
           height: "168px",
@@ -29,19 +30,215 @@ const Header = () => {
             position: "relative"
           }}
         >
-          zxc
+          <img
+            style={{
+              width: "227px",
+              height: "98px"
+            }}
+            src={require("../../../assets/img/logo.png").default}
+          />
+          <div style={{ position: "absolute", right: "0px", top: "78px" }}>
+            <span
+              style={{
+                display: "inline-block",
+                border: "3px solid #ff0000",
+                width: "342px"
+              }}
+            >
+              <input
+                style={{
+                  width: "300px",
+                  lineHeight: "22px",
+                  height: "22px",
+                  border: "none",
+                  background: "none",
+                  marginLeft: "5px"
+                }}
+                placeholder="스네이크헤드"
+              />
+              <div></div>
+            </span>
+          </div>
         </div>
         <div
           style={{
             margin: "20px 0 0px 0",
-            clear: "both",
-            borderBottom: "0px solid #ff0000",
-            borderTop: "0px solid #ff0000",
-            background: "#ff0000",
+            border: "0px",
             height: "40px"
           }}
         >
-          qwe
+          <div
+            style={{
+              width: "1200px",
+              margin: "0 auto",
+              position: "relative",
+              zIndex: 90,
+              height: "100%"
+            }}
+          >
+            <table style={{ width: "100%", height: "100%" }}>
+              <colgroup>
+                <col width="190px" />
+                <col width="*" />
+                <col width="*" />
+                <col width="*" />
+                <col width="*" />
+                <col width="*" />
+                <col width="*" />
+                <col width="*" />
+                <col width="*" />
+                <col width="*" />
+                <col width="*" />
+              </colgroup>
+              <tbody>
+                <tr>
+                  <td
+                    style={{
+                      background: "#ff0000",
+                      borderRight: "1px solid black",
+                      color: "white",
+                      fontWeight: "bold",
+                      boxSizing: "border-box",
+                      letterSpacing: "-1px",
+                      fontSize: "11px"
+                    }}
+                  >
+                    전체 카테고리
+                  </td>
+                  <td
+                    style={{
+                      background: "#ff0000",
+                      borderRight: "1px solid black",
+                      color: "white",
+                      fontWeight: "bold",
+                      boxSizing: "border-box",
+                      letterSpacing: "-1px",
+                      fontSize: "11px"
+                    }}
+                  >
+                    자주찾는 품목
+                  </td>
+                  <td
+                    style={{
+                      background: "#ff0000",
+                      borderRight: "1px solid black",
+                      color: "white",
+                      fontWeight: "bold",
+                      boxSizing: "border-box",
+                      letterSpacing: "-1px",
+                      fontSize: "11px"
+                    }}
+                  >
+                    브랜드 보기
+                  </td>
+                  <td
+                    style={{
+                      background: "#ff0000",
+                      borderRight: "1px solid black",
+                      color: "white",
+                      fontWeight: "bold",
+                      boxSizing: "border-box",
+                      letterSpacing: "-1px",
+                      fontSize: "11px"
+                    }}
+                  >
+                    고무보트
+                  </td>
+                  <td
+                    style={{
+                      background: "#ff0000",
+                      borderRight: "1px solid black",
+                      color: "white",
+                      fontWeight: "bold",
+                      boxSizing: "border-box",
+                      letterSpacing: "-1px",
+                      fontSize: "11px"
+                    }}
+                  >
+                    콤비보트
+                  </td>
+                  <td
+                    style={{
+                      background: "#ff0000",
+                      borderRight: "1px solid black",
+                      color: "white",
+                      fontWeight: "bold",
+                      boxSizing: "border-box",
+                      letterSpacing: "-1px",
+                      fontSize: "11px"
+                    }}
+                  >
+                    선외기
+                  </td>
+                  <td
+                    style={{
+                      background: "#ff0000",
+                      borderRight: "1px solid black",
+                      color: "white",
+                      fontWeight: "bold",
+                      boxSizing: "border-box",
+                      letterSpacing: "-1px",
+                      fontSize: "11px"
+                    }}
+                  >
+                    가이드모터
+                  </td>
+                  <td
+                    style={{
+                      background: "#ff0000",
+                      borderRight: "1px solid black",
+                      color: "white",
+                      fontWeight: "bold",
+                      boxSizing: "border-box",
+                      letterSpacing: "-1px",
+                      fontSize: "11px"
+                    }}
+                  >
+                    어탐기
+                  </td>
+                  <td
+                    style={{
+                      background: "#ff0000",
+                      borderRight: "1px solid black",
+                      color: "white",
+                      fontWeight: "bold",
+                      boxSizing: "border-box",
+                      letterSpacing: "-1px",
+                      fontSize: "11px"
+                    }}
+                  >
+                    트레일러
+                  </td>
+                  <td
+                    style={{
+                      background: "#ff0000",
+                      borderRight: "1px solid black",
+                      color: "white",
+                      fontWeight: "bold",
+                      boxSizing: "border-box",
+                      letterSpacing: "-1px",
+                      fontSize: "11px"
+                    }}
+                  >
+                    견인장치
+                  </td>
+                  <td
+                    style={{
+                      background: "#ff0000",
+                      borderRight: "1px solid black",
+                      color: "white",
+                      fontWeight: "bold",
+                      boxSizing: "border-box",
+                      letterSpacing: "-1px",
+                      fontSize: "11px"
+                    }}
+                  >
+                    게시판 목록
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </header>
